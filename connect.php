76534523083c21436019758914009b499665b5af
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-03-08 15:59:09 -0600

    added query that shows all entries in products upon connect

--- connect.php
+++ connect.php
@@ -3,8 +3,32 @@
     $pass = '';
     $db = 'inventoryengineering';
 
-    $dbconnection = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+    //connect to database
+    $conn = mysqli_connect('localhost', $user, $pass, $db);
 
+    //check connection. NOT WORKING. If successful, $conn returns true
+    if(!$conn){ // dot . is for concatenation
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    //write query for product table
+    $sql = 'SELECT upc, on_hand, product_name FROM product';
+
+    //make query & get result
+    $result = mysqli_query($conn, $sql);
+
+    //fetch the resulting rows as an array
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+    //clear $result from memory
+    mysqli_free_result($result);
+
+    //close connection
+    mysqli_close($conn);
+
+    print_r($products);
+
+    //$dbconnection = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
     //echo "Connection echo line";
 ?>
 
