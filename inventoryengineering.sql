e7b2282255fc6961318985de726fc07943e3e1d3
eman125 <42151087+eman125@users.noreply.github.com>
2022-03-16 21:47:24 -0500

    added user table to database
    inventoryengineering.sql database file has been updated to include the user table

--- inventoryengineering.sql
+++ inventoryengineering.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 15, 2022 at 02:09 AM
+-- Generation Time: Mar 17, 2022 at 03:45 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -62,6 +62,29 @@ CREATE TABLE `stocked_product` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `access_level` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `access_level`) VALUES
+(1, 'ShipmentReceiver', 'T3X@$', 'receiver@mail.com', 2),
+(2, 'Stocker', 'Star$$in3ye', 'stocker@mail.com', 1),
+(3, 'CheckoutEmployee', 'cH@mEle0n9', 'checkout@mail.com', 3);
+
 --
 -- Indexes for dumped tables
 --
@@ -84,6 +107,22 @@ ALTER TABLE `product`
 ALTER TABLE `stocked_product`
   ADD KEY `FK_stocked_product_product` (`upc`),
   ADD KEY `FK_stocked_product_location` (`location_name`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
