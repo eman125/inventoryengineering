475a398f2439707a0a93754c4ad7b9d929a69ad7
PedroG1337 <99843073+PedroG1337@users.noreply.github.com>
2022-03-30 17:08:54 -0500

    Update login.php
    fixed login being stuck in wrapper

--- login.php
+++ login.php
@@ -6,23 +6,23 @@
         <meta name="viewport" content="width=device-width, initial scale=1.0">
     </head>
     
-    <body> 
-       <div id="wrapper">
+<body>
+	<div id="wrapper">
             <nav>
                 <div class="navlinks">
                     <a class="logo" href="index.php">IE</a>
-                   <a href="index.php">Home</a>
+                    <a href="index.php">Home</a>
 					<a href="login.php">Login</a>
                 </div>
             </nav>
-                    <div>
+	</div>
 
-</head> <body> <form action="main_menu.php" method="post"> 
-<p> <label for="userName">User Name:</label> <input type="text" name="userName" id="userName"> </p> 
-<p> <label for="userpassword">Password:</label> <input type="text" name="userpassword" id="userpassword"> </p> 
-<input type="submit" value="Submit"> </form> 
-</div>
-            
+		<form action="main_menu.php" method="post"> 
+		<p> <label for="userName">User Name:</label> <input type="text" name="userName" id="userName"> </p> 
+		<p> <label for="userpassword">Password:</label> <input type="text" name="userpassword" id="userpassword"> </p> 
+		<input type="submit" value="Submit"> </form>
+
+	</div>            
             <footer id="foot">
                 <div class="navlinks">
                     <h4>Emmanuel Huitron, Pedro Gonzalez, Kelsey Houghton, Tracey Taylor</h4>
@@ -30,15 +30,5 @@
                     <i>Copyright © Us 2022</i>
                 </div>
             </footer>
-        </div>
-
-            <footer id="foot">
-                <div class="navlinks">
-                    <h4>Emmanuel Huitron, Pedro Gonzalez, Kelsey Houghton, Tracey Taylor</h4>
-                    <a href="mailto:temporary@notyet.com"> temporary@notyet.com</a><br>
-                    <i>Copyright © Us 2022</i>
-                </div>
-            </footer>
-        </div>
-    
+    </div>    
 </body></html>
