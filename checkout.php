0c09cd70f444bd814c881a53b35e2df796307161
Emmanuel Huitron <emanlahdy125@gmail.com>
2022-04-08 16:11:33 -0500

    created checkout.php file

---
+++ checkout.php
@@ -0,0 +1,87 @@
+<?php
+	$user =  'root';
+    $password = '';
+    $dbname = 'inventoryengineering';
+    $conn = new mysqli("localhost",$user,$password ,$dbname);
+
+	//1st dimension is product, 2nd is for amount to be sold
+	$productArray = array(array());
+
+	// Check connection
+    if ($conn -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $conn -> connect_error;
+        exit();
+    }
+
+	if(isset($_POST['submit']))
+	{
+		//checks if input value is numeric before running sql query
+		if(is_numeric($_POST['upc']))
+		{
+			$query = 'SELECT upc, on_hand, product_name FROM product WHERE upc=' . $_POST['upc'] . ';';
+
+            //uses queary to get results
+			$query_run = mysqli_query($conn,$query);
+
+            while($row = mysqli_fetch_array($query_run))
+			{
+				$upcVar = $row['upc'];
+				$onHandVar = $row['on_hand'];
+				$productNameVar = $row['product_name'];
+			}
+		}
+		else
+			$confirmText = "please enter numeric value";
+
+
+        //close connection
+        mysqli_close($conn);
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Search</title>
+	<link href="inventory_files/main.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial scale=1.0">
+</head>
+
+<body>
+<div id="wrapper">
+	<nav>
+		<div class="navlinks">
+            <a class="logo" href="index.php">IE</a>
+            <a href="index.php">Home</a>
+			<a href="login.php">Login</a>
+        </div>
+	</nav>
+
+	<main id="twocolumn">
+		<div id="leftcolumn">
+			<h3>Inventory Database: Checkout. Lookup by UPC</h3>
+			<form action="" method="POST">
+				<!--if type is "number", max/minlength don't work, php code checks for numeric-->
+                <label>upc:</label>
+				<input type="text" maxlength="13" minlength="12" name="upc" required/> <br/>
+				<input type="submit" name="submit" value="Submit">
+			</form>
+        </div>
+
+		<div>
+			<p><?php echo $productName; ?></p><br>
+            <p><?php echo ''; ?></p>
+		</div>
+	</main>
+
+	<footer id="foot">
+        <div class="navlinks">
+            <h4>Emmanuel Huitron, Pedro Gonzalez, Kelsey Houghton, Tracey Taylor</h4>
+            <a href="mailto:temporary@notyet.com"> temporary@notyet.com</a><br>
+            <i>Copyright © Us 2022</i>
+        </div>
+    </footer>
+</div>
+</body></html>
