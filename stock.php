1cf499ae9134f943e733f59595e451bfc5808ee1
eman125 <emanlahdy125@gmail.com>
2022-04-08 11:54:17 -0500

    reformatted stock.php

--- stock.php
+++ stock.php
@@ -1,65 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title>Products</title>		
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script src="inventory_files/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="inventory_files/style.css" />
-        <link href="inventory_files/main.css" rel="stylesheet">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial scale=1.0">
-    </head>    
-	<body> 
-	<div id="wrapper">
+	<title>Products</title>
+	<link href="inventory_files/main.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial scale=1.0">
+</head>
+
+<body>
+<div id="wrapper">
 	<nav>
 		<div class="navlinks">
-		<a class="logo" href="index.php">IE</a>
-		<a href='index.php'>Home</a> 		
-		<a href='login.php'>Login</a>
-		<a href='maint_menu.php'>Maintenance Menu</a>
-		</div>
+            <a class="logo" href="index.php">IE</a>
+            <a href="index.php">Home</a>
+			<a href="login.php">Login</a>
+        </div>
 	</nav>
-		<div>
 
-		<br />
-		<br />
-		<br />
-
+	<main id="twocolumn">
+		<div id="leftcolumn">
 <?php
-require_once('connect.php');
-session_start();
-if (isset($_SESSION['userName'])&&isset($_SESSION['userpassword']))
-{
+    require_once('connect.php');
+    session_start();
+    
+    if (isset($_SESSION['userName'])&&isset($_SESSION['userpassword']))
+    {
+        $sql="SELECT `username`, `password` FROM user WHERE access_level IN (1,2,4) AND username  = '" . $_SESSION['userName'] . "' AND   password = '" . $_SESSION['userpassword'] . "'";		
+        $result = $conn-> query($sql);
 
-$sql="SELECT `username`, `password` FROM user WHERE access_level IN (1,2,4) AND username  = '" . $_SESSION['userName'] . "' AND   password = '" . $_SESSION['userpassword'] . "'";		
-$result = $conn-> query($sql);
-if ($result->num_rows > 0) 
-{
-    while ($row = $result->fetch_assoc())
-		{	
-$sqlstock = "SELECT * FROM stocked_product";
-$stockresultset = $conn-> query($sqlstock);
-echo "<table  id='stocked_product'>
-		<tr>
-		<th>UPC</th>
-		<th>Location Name</th>
-		<th>Quantity</th>
-		<th colspan='2'><a class='btn btn-secondary'  role='button' href = 'create_stocks.php'>Add Stock</a></th>
-	</tr>";
-while($stockrow = $stockresultset -> fetch_array(MYSQLI_ASSOC))
-   {
-		echo "<tr id='". $stockrow['id']."'>";
-		echo "<td>" . $stockrow['upc'] . "</td>";
-		echo "<td>" . $stockrow['location_name'] . "</td>";
-		echo "<td>" . $stockrow['quantity'] . "</td>";
-		echo "<td><a class='btn btn-primary' href = 'edit_stocks.php?id=" . $stockrow['id'] . "'>Edit</a></td>";
-		echo "<td><button class='btn btn-danger btn-sm remove'>Delete</button></td>";
-		echo "</tr>";
-  }
-echo "</table>";
+        if ($result->num_rows > 0) 
+        {
+            while ($row = $result->fetch_assoc())
+		    {	
+                $sqlstock = "SELECT * FROM stocked_product";
+                $stockresultset = $conn-> query($sqlstock);
+                echo "<table  id='stocked_product'>
+		            <tr>
+		            <th>UPC</th>
+		            <th>Location Name</th>
+		            <th>Quantity</th>
+		            <th colspan='2'><a class='btn btn-secondary'  role='button' href = 'create_stocks.php'>Add Stock</a></th>
+	            </tr>";
+                while($stockrow = $stockresultset -> fetch_array(MYSQLI_ASSOC))
+                {
+		            echo "<tr id='". $stockrow['id']."'>";
+		            echo "<td>" . $stockrow['upc'] . "</td>";
+		            echo "<td>" . $stockrow['location_name'] . "</td>";
+		            echo "<td>" . $stockrow['quantity'] . "</td>";
+		            echo "<td><a class='btn btn-primary' href = 'edit_stocks.php?id=" . $stockrow['id'] . "'>Edit</a></td>";
+		            echo "<td><button class='btn btn-danger btn-sm remove'>Delete</button></td>";
+		            echo "</tr>";
+                }
+                echo "</table>";
+            }
+	    }
 
-		}
-		}
-$result -> free_result();
-}
+        $result -> free_result();
+    }
 ?>
 <script type="text/javascript">    
     $(".remove").click(function(){
@@ -81,13 +79,19 @@ $result -> free_result();
         }
     });
 </script>
-           <footer id="foot">
-                <div class="navlinks">
-                    <h4>Emmanuel Huitron, Pedro Gonzalez, Kelsey Houghton, Tracey Taylor</h4>
-                    <a href="mailto:temporary@notyet.com"> temporary@notyet.com</a><br>
-                    <i>Copyright © Us 2022</i>
-                </div>
-            </footer>  
-           
-</body>
-</html>
+        </div>
+
+		<div>
+			<!--right column not being used-->
+		</div>
+	</main>
+
+	<footer id="foot">
+        <div class="navlinks">
+            <h4>Emmanuel Huitron, Pedro Gonzalez, Kelsey Houghton, Tracey Taylor</h4>
+            <a href="mailto:temporary@notyet.com"> temporary@notyet.com</a><br>
+            <i>Copyright © Us 2022</i>
+        </div>
+    </footer>
+</div>
+</body></html>
