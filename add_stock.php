cb2aac9a319563991f586bdb239f41d5b6fddf0b
TCTaylor <taylor_t02640@utpb.edu>
2022-04-26 18:55:29 -0500

    Update add_stock.php
    Fixed issue with inserting data. Added select (dropdown menu) to control input regarding location name. Fixed required attributes.

--- add_stock.php
+++ add_stock.php
@@ -1,8 +1,6 @@
 <?php
-	$user =  'root';
-    $password = '';
-    $dbname = 'inventoryengineering';
-    $conn = new mysqli("localhost",$user,$password ,$dbname);
+	require_once('connect.php');
+	session_start();
 
 	//calling variables
 	$confirmText = "";
@@ -18,10 +16,16 @@
 		//checks if input value is numeric before running sql query
 		if(is_numeric($_POST['upc']))
 		{
-            //make string with passed values
-            $passedVal = '\'' . $_POST['upc'] . '\', ' . $_POST['location_name'] . ', \'' . $_POST['quantity'] . '\'';
-
-			$query = 'INSERT INTO stocked_product (upc, location_name, quantity) VALUES (' . $passedVal . ');';
+            $upc= $_POST['upc'];
+			$locationName= $_POST['location_name'];
+			$quantity= $_POST['quantity'];
+			
+			$query = "INSERT INTO stocked_product (upc, location_name, quantity)
+			VALUES (
+			'$upc',
+			'$locationName',
+			'$quantity'
+			)";
 		
             if($conn->query($query) === TRUE)
             {
@@ -33,7 +37,7 @@
             }
 		}
 		else
-			$confirmText = "please enter numeric value";
+			$confirmText = "Please enter a numeric value.";
 
 
         //close connection
@@ -47,8 +51,10 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>Add Stock</title>
 	<link href="inventory_files/main.css" rel="stylesheet">
+	<!--<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">-->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial scale=1.0">
+
 </head>
 
 <body>
@@ -64,15 +70,20 @@
 
 	<main id="twocolumn">
 		<div id="leftcolumn">
-			<h3>Inventory Database: Enter UPC number, on hand number, and name</h3>
+			<h3>Inventory Database: Enter UPC number, location, and quantity</h3>
 			<form action="" method="POST">
 				<!--if type is "number", max/minlength don't work, php code checks for numeric-->
                 <label>upc:</label>
-				<input type="text" maxlength="13" minlength="12" name="upc" required/> <br/>
+				<input type="text" maxlength="13" minlength="12" name="upc" required = "required"/> <br/>
                 <label>location:</label>
-                <input type="text" maxlength="255" name="location_name" required/> <br/>
+                <select style="width: 178px; height: 20px; float: right;" type="text" name="location_name" required = "required" >
+					<option value="Aisle 1"> Aisle 1 </option>
+					<option value="Aisle 2"> Aisle 2 </option>
+					<option value="Aisle 3"> Aisle 3 </option>
+					<option value="Aisle 4"> Aisle 4 </option>
+				</select></br>
 				<label>quantity:</label>
-                <input type="number" pattern="[0-9]" name="quantity" required/> <br/>
+                <input type="number" pattern="[0-9]" name="quantity" required = "required"/> <br/></br>
 				<input type="submit" name="submit" value="Submit">
 			</form>
         </div>
